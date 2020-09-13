dca6479c8812291bf0883ab832a765b1289d6566
Rafael Comonfort <r.comonfort@outlook.com>
2020-09-13 00:43:01 -0500

    Latest changes

--- Assets/Scripts/LiquidTap.cs
+++ Assets/Scripts/LiquidTap.cs
@@ -6,21 +6,38 @@ public class LiquidTap : MonoBehaviour
 {
 	[SerializeField] private float timeBetweenDrops = .4f;
 	[SerializeField] private Rigidbody2D dropletPrefab = null;
+	[SerializeField] private int maxDrops = 100;
 
 	[SerializeField] private Vector2 outputDirection;
 	[SerializeField] private float initialSpeed = 10f;
 
+	private int dropsExpelled = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-		InvokeRepeating("SpawnDroplet", 0, timeBetweenDrops);
+		Init();
 		
     }
 
+	public void Init() {
+		dropsExpelled = 0;
+		InvokeRepeating("SpawnDroplet", 0, timeBetweenDrops);
+	}
+
 	private void SpawnDroplet() {
+
+		if (maxDrops > 0 && dropsExpelled == maxDrops)
+		{
+			CancelInvoke("SpawnDroplet");
+			return;
+		}
+
 		Quaternion spawnRot = Quaternion.LookRotation(Vector3.forward, Vector3.Cross(outputDirection, Vector3.forward));
 		Rigidbody2D droplet = Instantiate<Rigidbody2D>(dropletPrefab, transform.position, spawnRot);
 		droplet.AddForce(outputDirection*initialSpeed);
+
+		dropsExpelled++;
 	}
 
 	private void OnDrawGizmosSelected() {
---
+++ Assets/Scripts/RotatorObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorObject : MonoBehaviour
+{
+	[SerializeField] private float rpm = 60;
+
+    // Update is called once per frame
+    void Update()
+    {
+		float rotSpeed = rpm * 6 * Time.deltaTime; // deg / frame
+        transform.Rotate(Vector3.forward * rotSpeed, Space.Self);
+    }
+}
